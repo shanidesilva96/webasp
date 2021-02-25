802c4b6fe0f9ae1d42503f93b36de34ad39d4470
shanidesilva96 <53310861+shanidesilva96@users.noreply.github.com>
2021-02-25 17:13:51 +0530

    Create jsCodes.js

---
+++ Special Codes/jsCodes.js
@@ -0,0 +1,4 @@
+//Click Submit Button in JS
+<button type="submit" name="submitbutton" value="XLS" class="btn btn-success btn-width">Export To Excel</button>
+
+document.getElementById('btnLoad2').click();
