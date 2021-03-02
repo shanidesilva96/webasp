55e658ccf4fc630a844f755221554eb870e1c12c
shanidesilva96 <53310861+shanidesilva96@users.noreply.github.com>
2021-03-02 12:39:12 +0530

    Update jsCodes.js

--- Special Codes/jsCodes.js
+++ Special Codes/jsCodes.js
@@ -2,3 +2,9 @@
 <button type="submit" name="submitbutton" value="XLS" class="btn btn-success btn-width">Export To Excel</button>
 
 document.getElementById('btnLoad2').click();
+
+
+
+$('select').select2({
+  minimumInputLength: 3 // only start searching when the user has input 3 or more characters
+});
