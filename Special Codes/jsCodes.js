ba9f9b23ccd06265b1e6e0709134d3b56cd9d5ef
shanidesilva96 <53310861+shanidesilva96@users.noreply.github.com>
2021-03-03 10:12:03 +0530

    Update jsCodes.js

--- Special Codes/jsCodes.js
+++ Special Codes/jsCodes.js
@@ -8,3 +8,29 @@ document.getElementById('btnLoad2').click();
 $('select').select2({
   minimumInputLength: 3 // only start searching when the user has input 3 or more characters
 });
+
+
+//ADD ROW in to table with .DATATABLE
+$(document).ready(function () {
+    var t = $('#facTbl').DataTable();
+    var counter = 1;
+
+    $('#addRow').on('click', function () {
+        t.row.add([
+            counter + '.1',
+            counter + '.2',
+            counter + '.3',
+            counter + '.4',
+            counter + '.5',
+            counter + '.6',
+            counter + '.7',
+            counter + '.8',
+            counter + '.9'
+        ]).draw(false);
+
+        counter++;
+    });
+
+
+    $('#addRow').click();
+});
